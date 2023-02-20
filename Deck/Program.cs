a6f0cc5e16699b070c1970cac27e7b84bbdb232c
Desjardins, Mathew Ryan <DESJARDINSM@etsu.edu>
2023-02-20 14:52:09 -0500

    Add project files.

---
+++ Deck/Program.cs
@@ -0,0 +1,20 @@
+﻿using Deck;
+Console.WriteLine("This is the default card!");
+Console.WriteLine(new Card() + "\n");
+
+//I did not create the repeating card ask...
+
+//This is the deck of cards. I did not create a method here.
+
+Console.WriteLine("A deck of cards!");
+Card[] deck = new Card[52];
+
+for (int i = 0; i < deck.Length; i++)
+{
+    deck[i] = new Card(i);
+}
+
+foreach (var item in deck)
+{
+    Console.WriteLine(item);
+}
